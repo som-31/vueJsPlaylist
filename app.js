59b6059f9d1577e4196aba28cb9a26105873fb40
Somnath Jadhav <somnath.jadhav@lapr075.successivetech.com>
2020-01-30 12:07:13 +0530

    updated lesson 12

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 new Vue({
     el : "#vue-app",
     data : {
-      error : false,
-      success: false
+    characters : ['name1', 'name2','name3', 'name4'],
+    ninjas : [
+        {name : 'name1', age : 23},
+        {name : 'name2', age : 23},
+        {name : 'name3', age : 23},
+        {name : 'name4', age : 23},
+    ]
     },
     methods : {
 
